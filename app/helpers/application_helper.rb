afa8048417ace594cb421e43b1a80bf27ca07019
Jamob <tspurdle18@gmail.com>
2012-05-31 14:22:54 -0400

    Finished static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # so that I don't have to type out the entire bloody title every time
+  def full_title(page_title)
+    base_title = "MSPS"
+    if page_title.blank?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
