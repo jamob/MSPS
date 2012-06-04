d637a1638237dda89a3ee5e0fd077f3ce7e8d751
Jamob <tspurdle18@gmail.com>
2012-06-04 10:50:08 -0400

    :q

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -2,9 +2,13 @@ class FacebookController < ApplicationController
   
   def index
     YAML::load(File.open(Rails.root.join("config/facebook.yml")))
+    my_app = FbGraph::Application.new(config['production']['324090001000391'])
+    acc_tok = my_app.get_access_token(config['production']['785117f9e125ab08957deb100fc2cbab'])
     
     
-    @page = FbGraph::Page.fetch('324090001000391') 
+    @page = FbGraph::Page.new(config['production']['324090001000391'], :access_token => acc_tok).fetch
+    
+    @posts = @page.messages
     
   end
 end
